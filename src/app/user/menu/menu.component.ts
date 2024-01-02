c110182d69ff8b3d5c88ebb0c24c1d1b9478ea02
loganathan04 <loganathan.v@oasyscybernetics.in>
2024-01-02 12:08:36 +0530

    first commit

--- src/app/user/menu/menu.component.ts
+++ src/app/user/menu/menu.component.ts
@@ -9,7 +9,23 @@ export class MenuComponent implements OnInit {
 
   constructor() { }
 
+  imageData = [
+    {
+      imagePath: '../../../assets/vegetable2.jpg'
+    },
+    {
+      imagePath: '../../../assets/vegetable3.jpg'
+    }, {
+      imagePath: '../../../assets/vegetable4.jpg'
+    },
+    {
+      imagePath: '../../../assets/vegetable6.jpg'
+    }
+   
+  ]
+
   ngOnInit() {
+    console.log("image array ",this.imageData)
   }
 
 }
