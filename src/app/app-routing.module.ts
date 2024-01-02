5d8ee584edd49cd23289bd9cf2c81e36835820b6
loganathan04 <loganathan.v@oasyscybernetics.in>
2024-01-02 12:17:59 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {
     path:'',
-    component:LoginComponent
+    component:UserComponent
   },
   {
     path: 'user',
