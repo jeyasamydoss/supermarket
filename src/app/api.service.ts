f3894f54441fef7b317af582b821b5d111fac5ad
nithish1998 <nithish09091998@gmail.com>
2024-01-04 14:19:51 +0530

    after edit and delete

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -19,9 +19,17 @@ public post(path:any,data:any):Observable<any>{
   return this.http.post<any>(this.baseUrl+path,data);
 }
 
+public getid(path:any):Observable<any>{
+  return this.http.get<any>(this.baseUrl+path);
+}
+
 public put(path:any,data:any):Observable<any>{
   return this.http.put<any>(this.baseUrl+path,data);
 }
+public delete(path:any):Observable<any>{
+  return this.http.delete<any>(this.baseUrl+path);
+}
+
 
 
 }
