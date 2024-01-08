6812b4df073aec1abfbcc5e1680ecb1f9dab2974
Ponakil <ponakil38@gmail.com>
2024-01-08 20:50:48 +0530

    change ui

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,9 +8,11 @@ import { ApiService } from '../api.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  title = 'login';
   public userName: any;
   public password: any;
+  public email: any;
+  public mobile: any;
 
   constructor(private router: Router, private api: ApiService) { }
 
@@ -19,7 +21,7 @@ export class LoginComponent implements OnInit {
 
     if (storedUser) {
       const user = JSON.parse(storedUser);
-      
+
       if (user.role === 'ROLE_USER') {
         this.router.navigate(['']);
       } else if (user.role === 'ROLE_ADMIN') {
@@ -32,11 +34,11 @@ export class LoginComponent implements OnInit {
     let payload: any = {};
     payload['userName'] = this.userName;
     payload['password'] = this.password;
-    
+
     this.api.post('user/login', payload).subscribe((res) => {
       if (res.role === 'ROLE_USER' || res.role === 'ROLE_ADMIN') {
         localStorage.setItem('user', JSON.stringify(res));
-        
+
         if (res.role === 'ROLE_USER') {
           this.router.navigate(['']);
         } else if (res.role === 'ROLE_ADMIN') {
@@ -46,5 +48,29 @@ export class LoginComponent implements OnInit {
         alert('User Not Found');
       }
     });
+  }
+  register() {
+    let payload: any = {};
+    payload['userName'] = this.userName;
+    payload['password'] = this.password;
+    payload['email'] = this.email;
+    payload['mobile'] = this.mobile;
+    payload['role'] = "ROLE_USER"
+    this.api.post('user', payload).subscribe((res) => {
+      this.getLogin();
+
+    });
+
+  }
+  goHome() {
+    this.router.navigate(['']);
+  }
+  getLogin() {
+    this.title = 'login';
+    this.userName = null;
+    this.password = null;
+  }
+  getRegister() {
+    this.title = 'register';
   }
 }
