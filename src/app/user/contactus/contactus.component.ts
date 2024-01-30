598a3a5e81b622b01da452d6e3f0ecce3127dd00
jeyasamydoss <jeyasamydoss@gmail.com>
2024-01-30 17:44:15 +0530

    contact

--- src/app/user/contactus/contactus.component.ts
+++ src/app/user/contactus/contactus.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
+import { SnackbarServiceService } from 'src/app/common/snackbarService.service';
 
 @Component({
   selector: 'app-contactus',
@@ -9,25 +11,48 @@ import { ApiService } from 'src/app/api.service';
 })
 export class ContactusComponent implements OnInit {
 
-  constructor(private service:ApiService) { }
-data:any;
+  constructor(private formBuilder: FormBuilder, private service: ApiService,
+    private snackbar: SnackbarServiceService, private router: Router) { }
+
+  data: any;
+  contactForm: FormGroup;
+
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile_num: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  get subject() {
+    return this.contactForm.get('subject');
+  }
+  get message() {
+    return this.contactForm.get('message');
+  }
+  get mobile_num() {
+    return this.contactForm.get('mobile_num');
   }
   gotoadmin() {
-this.post();
+    this.post();
   }
-contact=new FormGroup({
-  name:new FormControl,
-  email:new FormControl,
-  subject:new FormControl,
-  message:new FormControl
-})
-post(){
-  this.service.post("api/contact-us",this.contact.value).subscribe((res)=>{
-this.data=res;
-console.log("query sended",res);
-this.contact.reset();
-  })
-}
 
+  post() {
+    this.service.post("api/contact-us", this.contactForm.value).subscribe((res) => {
+      this.data = res;
+      console.log("query sended", res);
+      this.contactForm.reset();
+      this.snackbar.showSuccessMessage("Your Message Send SuccessFully !")
+      this.router.navigate(['']);
+    });
+  }
 }
